43cae73567a36ae78826a27544a347febe69f757
Nataliia Balashova <nataliiabalashova@Nataliias-MacBook-Air.local>
2022-05-24 23:16:22 +0300

    added gitignore, refactored rs files location, updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ mod draw;
 mod snake;
 mod game;
 
-
-
-
 use piston_window::*;
 use piston_window::types::Color;
 
@@ -15,8 +12,6 @@ use crate::game::Game;
 use crate::draw::to_coord_u32;
 
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
-
-
 
 fn main() {
     let (width, height) = (30, 30);
